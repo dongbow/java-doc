d8ce14901dd7c206953af29830522af134b00fd8
dongbow <dongbow1@gmail.com>
2020-07-13 16:41:00 +0800

    fix: npe

--- src/main/java/com/github/doc/util/StringUtil.java
+++ src/main/java/com/github/doc/util/StringUtil.java
@@ -1,5 +1,7 @@
 package com.github.doc.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * @author wangdongbo
  * @since 2020/3/24.
@@ -7,6 +9,9 @@ package com.github.doc.util;
 public class StringUtil {
 
     public static String encodeStr(String content) {
+        if (StringUtils.isBlank(content)) {
+            return content;
+        }
         return content.replace("<", "&#60;").replace(">", "&#62;");
     }
 
